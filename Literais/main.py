4d411734cdebc615c3ae5a9b95f1e8c6abf62781
Miguel Marcondes <miguelmarcondesloureiro2305@gmail.com>
2025-05-02 11:17:22 -0300

    feat: parte 1 literais

--- Literais/main.py
+++ Literais/main.py
@@ -1,1 +1,17 @@
-print("Dados literais")
+print("Dados literais: ")
+
+print("2", "String")  # String, o 2 é um elemento textual
+print(2, "Numero int")    # Int, o 2 representa um numero inteiro. Numeros desprovidos da parte fracionaria
+            # Float, numero capaz de representar a parte fracionaria
+
+# Se um número inteiro for precedido por um prefixo 0O ou 0o, ele será tratado como um valor octal.
+print(0o123, "Octal")  # Resultado igual a 83
+
+# Se um número inteiro for precedido por um prefixo 0x ou 0X, ele será tratado como um valor hexadecimal .
+print(0x123, "Hexadecimal")  # Resultado igual a 291
+
+
+# Float
+    # Numero capaz de representar a parte fracionaria
+        # O ponto é o elemento separador dos numeros
+print(-0.5, "Float")
