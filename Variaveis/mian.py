6d0a24d008e5ecfe2cf09236bbd12b4d59b7ac52
Miguel Marcondes <miguelmarcondesloureiro2305@gmail.com>
2025-05-02 13:33:13 -0300

    feat: variaveis completo

---
+++ Variaveis/mian.py
@@ -0,0 +1,27 @@
+print("Variaveis")
+
+var = 1  # Crio uma variavel e coloco manualmente um valor nela
+print(var)  # Impressão da variavel
+
+# Exemplo mais detalhado
+Var = 1
+account_balance = 1000.0
+client_name = 'John Doe'
+print(Var, account_balance, client_name)
+print(Var)
+
+# Posso usar + pra concatenar 
+varr = "3.8.5"
+print("Versão Python: " + varr)
+
+# Operação com variavel
+var = 1
+print(var)  # Resultado 1
+var = var + 1
+print(var)  # Resultado 2
+
+# Operação matematica com variaveis 
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
