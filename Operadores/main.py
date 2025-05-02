57695d4f51d270e2649c479646f7f8515b835d7f
Miguel Marcondes <miguelmarcondesloureiro2305@gmail.com>
2025-05-02 13:00:29 -0300

    feat: Operadores completo

--- Operadores/main.py
+++ Operadores/main.py
@@ -1,1 +1,36 @@
-print("gre")
+print("Operadores")
+
+# Exponenciação
+print(2 ** 3)  # Resultado 8
+print(2 ** 3.)  # Resultado 8.0
+
+# Multiplicação
+print(2 * 3)  # Resultado 6
+print(2 * 3.)  # Resultado 6.0
+
+# Divisão
+print(6 / 3)  # Resultado 2
+print(6 / 3.)  # Resultado 2.0
+
+# Divisão de número inteiro
+print(6 // 3)  # Resultado 2
+print(6 // 3.)  # Resultado 2.0
+print(6 // 4)  # Resultado 1
+print(6. // 4)  # Resultado 1.0
+print(-6 // 4)  # Resultado -2
+print(6. // -4)  # Resultado -2.0
+# o arredondamento sempre vai para o número inteiro menor.
+
+# Restante
+# O resultado do operador % é o restante após a divisão do número inteiro
+print(14 % 4)  # Resultado 2
+print(12 % 4.5)  # Resultado 3.0
+
+# Adição
+print(-4 + 4)  # Resultado 0
+print(-4. + 8)  # Resultado 4.0
+
+# Subtração
+print(-4 - 4)  # Resultado -8
+print(4. - 8)  # Resultado -4.0
+
