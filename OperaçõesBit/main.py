41478cfad3420cf39c651ab0e9134dc57fc3d913
Miguel Marcondes <miguelmarcondesloureiro2305@gmail.com>
2025-05-09 21:33:18 -0300

    feat: anotações  de operações de bit

--- OperaçõesBit/main.py
+++ OperaçõesBit/main.py
@@ -1,1 +1,17 @@
 print("Logica e operações de bit")
+
+# and
+# false + false = false
+# false + true = false
+# true + false = false
+# true + true = true
+
+# or
+# false + false = false
+# false + true = true
+# true + false = true
+# true + true = true
+
+# not
+# true = false
+# false = true
