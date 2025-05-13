6b0265d7684492ace1bead1edd0104fbb36b6fb7
Miguel Marcondes <miguelmarcondesloureiro2305@gmail.com>
2025-05-13 18:31:32 -0300

    feat: update return

--- Return/main.py
+++ Return/main.py
@@ -1,1 +1,10 @@
 print("Return")
+
+# Exemplo pratico:
+def boring_function():
+    print("'Modo de tédio' ON.")
+    return
+
+print("Esta lição é interessante!")
+boring_function()
+print("Essa aula é chata...")
