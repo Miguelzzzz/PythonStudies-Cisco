a15fce4610e57e77e85e6270a83cc20ddcfdb657
Miguel Marcondes <miguelmarcondesloureiro2305@gmail.com>
2025-05-18 16:35:51 -0300

    feat: conteudo de modulos e pacotes

---
+++ Fundamentos_Python2/ModulosPacotes/main.py
@@ -0,0 +1,3 @@
+print("Modulos e Pacotes")
+
+# Um pacote é um contêiner que permite o agrupamento de diversos módulos relacionados sob um nome comum. Tal contêiner pode ser distribuído em seu estado atual (como um lote de arquivos implementados em uma subárvore de diretório) ou pode ser compactado em um arquivo zip.
