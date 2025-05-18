fa849e978e75e96bedf3827e7a050983b267eacc
Miguel Marcondes <miguelmarcondesloureiro2305@gmail.com>
2025-05-18 15:09:36 -0300

    feat: conteudo de modulo

--- Fundamentos_Python2/Modulo/main.py
+++ Fundamentos_Python2/Modulo/main.py
@@ -1,1 +1,19 @@
 print("Modulos Introdução")
+
+# Posso usar a palavra import pra tornar possivel o uso de bibliotecas
+
+# Exemplo pratico:
+import math  # Importo a biblioteca
+print(math.sin(math.pi/2))  # Digo o nome do modulo que vou usar "." o nome da função/entidade que quero
+
+# Forma mais simplificada
+from math import sin, pi
+
+print(sin(pi/2))
+
+# Essa importação ira importar tudo que contem no modulo falado
+# from module import *
+
+# Usar "as" nas importações para alterar nomes de bibliotecas/modulos
+    # import math as m
+    # print(m.sin(m.pi/2))
